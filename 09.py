343018a61c027bb08c1cad4f73fb4d37d32cb668
Logan-Rose <logan.rose0329@gmail.com>
2023-12-09 13:11:33 -0500

    Day 9 part 2

--- 09.py
+++ 09.py
@@ -1,5 +1,5 @@
 inputs = open("inputs_09.txt", "r").read().split('\n')[0:-1]
-
+# Part One
 histories = []
 total = 0
 for history in inputs:
@@ -14,5 +14,22 @@ for history in inputs:
     for i in range(2, len(layers)):
         layers[i].append(layers[i-1][-1] + layers[i][-1])
     total += layers[-1][-1]
+
+print(total)
+
+histories = []
+total = 0
+for history in inputs:
+    topLayer = [int(x) for x in history.split()]
+    layers = [topLayer]
+    while len(layers) == 1 or len(layer) > 0:
+        layer = []
+        for i in range(1, len(layers[-1])):
+            layer.append(layers[-1][i] - layers[-1][i-1])
+        layers.append(layer)
+    layers = layers[::-1]
+    for i in range(2, len(layers)):
+        layers[i].insert(0, layers[i][0] - layers[i-1][0])
+    total += layers[-1][0]
 
 print(total)
